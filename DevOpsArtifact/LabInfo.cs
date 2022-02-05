e66dad4dd8b55b080a6d8d8ff9b12fe4193aa28e
LucasLosano <losano3@hotmail.com>
2022-02-04 22:26:55 -0300

    Temp

---
+++ DevOpsArtifact/LabInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsArtifact
+{
+    public class LabInfo
+    {
+        public string Version
+        {
+            get
+            {
+                return "First test";
+            }
+        }
+    }
+}
